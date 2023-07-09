a6542b56c66ffa89410e050a5effa20948d64b26
Keyvan Hosseini <skhprogramming@gmail.com>
2023-07-09 14:57:52 +0330

    Refactor GameGrid component

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
         />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" paddingX={5}>
+        <GridItem area="aside" paddingX={5} paddingTop={10}>
           <GenreList
             selectedGenre={gameQuery.genre}
             onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -10,7 +10,7 @@ const GameHeading = ({ gameQuery }: Props) => {
     gameQuery.genre?.name || ""
   } Games`;
   return (
-    <Heading as="h1" marginY={5} fontSize="5xl">
+    <Heading as="h1" marginY={5} fontSize="6xl">
       {heading}
     </Heading>
   );
