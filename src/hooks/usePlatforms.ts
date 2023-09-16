f8dfbeb4e18c469a828a374ccefab44abbdbb8ce
Keyvan Hosseini <skhprogramming@gmail.com>
2023-09-16 17:31:36 +0330

    Refactored Platforms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,23 @@
 import platforms from "../data/platforms";
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client.ts";
+import { FetchResponse } from "./useData.ts";
 
-// interface Platform {
-//   id: number;
-//   name: string;
-//   slug: string;
-// }
+interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 
-const usePlatforms = () => ({ data: platforms, error: null });
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatforms;
