36b302d3209da706011e29e12addda547b2c1a8c
Keyvan Hosseini <skhprogramming@gmail.com>
2023-09-16 20:07:45 +0330

    Refactored to simplify time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,6 +3,7 @@ import { FetchResponse } from "../services/api-client.ts";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client.ts";
 import { Platform } from "./usePlatforms.ts";
+import ms from "ms";
 
 export interface Game {
   id: number;
@@ -32,7 +33,7 @@ const useGames = (gameQuery: GameQuery) =>
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
-    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    staleTime: ms("24h"),
   });
 
 export default useGames;
